f68444bc69c0b5c193c0d80caf5dcec9ba514689
Hobin Yoon <hobinyoon@gmail.com>
2016-05-04 12:43:31 -0400

    new AMIs

--- ec2/RunAndMonitorEc2Inst.py
+++ ec2/RunAndMonitorEc2Inst.py
@@ -36,9 +36,9 @@ def Run(regions = ["us-east-1"], tag_name = None):
 class RunAndMonitor():
 	def __init__(self, region_name, tag_name):
 		if region_name == "us-east-1":
-			self.ami_id = "ami-87e404ea"
+			self.ami_id = "ami-3ff51552"
 		elif region_name == "us-west-1":
-			self.ami_id = "ami-3a1c655a"
+			self.ami_id = "ami-3b11685b"
 		else:
 			raise RuntimeError("Unexpected region %s" % region_name)
 		self.region_name = region_name
