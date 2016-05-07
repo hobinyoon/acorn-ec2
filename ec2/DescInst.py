a1ceabfe9f58163c5f1ff2bb350be90beae2bd11
Hobin Yoon <hobinyoon@gmail.com>
2016-05-07 14:49:07 +0000

    refactor. take tag_name argument.

---
+++ ec2/DescInst.py
@@ -0,0 +1,157 @@
+import boto3
+import os
+import pprint
+import sys
+import threading
+
+sys.path.insert(0, "%s/../util/python" % os.path.dirname(os.path.realpath(__file__)))
+import Cons
+import Util
+
+
+_fmt = "%-15s %10s %10s %13s %15s %15s %13s %20s"
+
+
+def Run(tag_name = None):
+	threads = []
+
+	sys.stdout.write("desc_instances:")
+	sys.stdout.flush()
+
+	regions = [
+			"us-east-1"
+			, "us-west-1"
+			, "us-west-2"
+			, "eu-west-1"
+			, "eu-central-1"
+			, "ap-southeast-1"
+			, "ap-southeast-2"
+			, "ap-northeast-2"
+			, "ap-northeast-1"
+			, "sa-east-1"
+			]
+
+	dis = []
+	for r in regions:
+		dis.append(DescInstPerRegion(r, tag_name))
+
+	for di in dis:
+		t = threading.Thread(target=di.Run)
+		threads.append(t)
+		t.start()
+
+	for t in threads:
+		t.join()
+	print ""
+
+	num_insts = 0
+	for di in dis:
+		num_insts += di.NumInsts()
+	if num_insts == 0:
+		ConsP("No instances found.")
+		return
+
+	print ""
+	ConsP(Util.BuildHeader(_fmt,
+		"Placement:AvailabilityZone"
+		" InstanceId"
+		" InstanceType"
+		" LaunchTime"
+		" PrivateIpAddress"
+		" PublicIpAddress"
+		" State:Name"
+		" Tag:Name"
+		))
+
+	for di in dis:
+		di.PrintResult()
+
+
+class DescInstPerRegion:
+	def __init__(self, region, tag_name):
+		self.region = region
+		self.tag_name = tag_name
+		self.exception = None
+
+	def Run(self):
+		try:
+			# http://boto3.readthedocs.io/en/latest/guide/session.html
+			session = boto3.session.Session()
+			boto_client = session.client("ec2", region_name=self.region)
+
+			if self.tag_name == None:
+				self.response = boto_client.describe_instances()
+			else:
+				self.response = boto_client.describe_instances(
+						Filters = [{
+							'Name': 'tag:Name',
+							'Values': [self.tag_name]
+							}]
+						)
+
+		except KeyError as e:
+			#ConsP("region=%s KeyError=[%s]" % (self.region, e))
+			self.exception = e
+
+		sys_stdout_write(" %s" % self.region)
+
+	def NumInsts(self):
+		if self.exception != None:
+			return 0
+		num = 0
+		for r in self.response["Reservations"]:
+			for r1 in r["Instances"]:
+				num += 1
+		return num
+
+	def PrintResult(self):
+		if self.exception != None:
+			ConsP("region=%s KeyError=[%s]" % (self.region, self.exception))
+			return
+
+		#ConsP(pprint.pformat(self.response, indent=2, width=100))
+
+		for r in self.response["Reservations"]:
+			for r1 in r["Instances"]:
+				tag_name = None
+				if "Tags" in r1:
+					for t in r1["Tags"]:
+						if t["Key"] == "Name":
+							tag_name = t["Value"]
+
+				ConsP(_fmt % (
+					_Value(_Value(r1, "Placement"), "AvailabilityZone")
+					, _Value(r1, "InstanceId")
+					, _Value(r1, "InstanceType")
+					, _Value(r1, "LaunchTime").strftime("%y%m%d-%H%M%S")
+					, _Value(r1, "PrivateIpAddress")
+					, _Value(r1, "PublicIpAddress")
+					, _Value(_Value(r1, "State"), "Name")
+					, tag_name
+					))
+
+
+def _Value(dict_, key):
+	if key == "":
+		return ""
+
+	if key in dict_:
+		return dict_[key]
+	else:
+		return ""
+
+
+_print_lock = threading.Lock()
+
+# Serialization is not needed in this file. Leave it for now.
+def ConsP(msg):
+	with _print_lock:
+		Cons.P(msg)
+
+
+def sys_stdout_write(msg):
+	with _print_lock:
+		sys.stdout.write(msg)
+		sys.stdout.flush()
+
+
--- ec2/desc-insts.py
+++ ec2/desc-insts.py
@@ -1,143 +1,17 @@
 #!/usr/bin/env python
 
-import boto3
-import os
-import pprint
 import sys
-import threading
 
-sys.path.insert(0, "%s/../util/python" % os.path.dirname(os.path.realpath(__file__)))
-import Cons
-import Util
-
-
-_fmt = "%-15s %10s %10s %13s %15s %15s %13s %20s"
-
-
-def RunDescInst():
-	threads = []
-
-	sys.stdout.write("desc_instances:")
-	sys.stdout.flush()
-
-	regions = [
-			"us-east-1"
-			, "us-west-1"
-			, "us-west-2"
-			, "eu-west-1"
-			, "eu-central-1"
-			, "ap-southeast-1"
-			, "ap-southeast-2"
-			, "ap-northeast-2"
-			, "ap-northeast-1"
-			, "sa-east-1"
-			]
-
-	dis = []
-	for r in regions:
-		dis.append(DescInst(r))
-
-	for di in dis:
-		t = threading.Thread(target=di.Run)
-		threads.append(t)
-		t.start()
-
-	for t in threads:
-		t.join()
-	print ""
-	print ""
-
-	ConsP(Util.BuildHeader(_fmt,
-		"Placement:AvailabilityZone"
-		" InstanceId"
-		" InstanceType"
-		" LaunchTime"
-		" PrivateIpAddress"
-		" PublicIpAddress"
-		" State:Name"
-		" Tag:Name"
-		))
-
-	for di in dis:
-		di.PrintResult()
-
-
-class DescInst:
-	def __init__(self, region):
-		self.region = region
-		self.exception = None
-
-	def Run(self):
-		try:
-			# http://boto3.readthedocs.io/en/latest/guide/session.html
-			session = boto3.session.Session()
-			boto_client = session.client("ec2", region_name=self.region)
-
-			self.response = boto_client.describe_instances(
-					#Filters = [{
-					#	'Name': 'tag:Name',
-					#	'Values': ['acorn-server']
-					#	}]
-					)
-		except KeyError as e:
-			#ConsP("region=%s KeyError=[%s]" % (self.region, e))
-			self.exception = e
-
-		sys_stdout_write(" %s" % self.region)
-
-	def PrintResult(self):
-		if self.exception != None:
-			ConsP("region=%s KeyError=[%s]" % (self.region, self.exception))
-			return
-
-		#ConsP(pprint.pformat(self.response, indent=2, width=100))
-
-		for r in self.response["Reservations"]:
-			for r1 in r["Instances"]:
-				tag_name = None
-				if "Tags" in r1:
-					for t in r1["Tags"]:
-						if t["Key"] == "Name":
-							tag_name = t["Value"]
-
-				ConsP(_fmt % (
-					_Value(_Value(r1, "Placement"), "AvailabilityZone")
-					, _Value(r1, "InstanceId")
-					, _Value(r1, "InstanceType")
-					, _Value(r1, "LaunchTime").strftime("%y%m%d-%H%M%S")
-					, _Value(r1, "PrivateIpAddress")
-					, _Value(r1, "PublicIpAddress")
-					, _Value(_Value(r1, "State"), "Name")
-					, tag_name
-					))
-
-
-def _Value(dict_, key):
-	if key == "":
-		return ""
-
-	if key in dict_:
-		return dict_[key]
-	else:
-		return ""
-
-
-_print_lock = threading.Lock()
-
-# Serialization is not needed in this file. Leave it for now.
-def ConsP(msg):
-	with _print_lock:
-		Cons.P(msg)
-
-
-def sys_stdout_write(msg):
-	with _print_lock:
-		sys.stdout.write(msg)
-		sys.stdout.flush()
+import DescInst
 
 
 def main(argv):
-	RunDescInst()
+	if len(argv) == 1:
+		DescInst.Run()
+	elif len(argv) == 2:
+		DescInst.Run(argv[1])
+	else:
+		print "Usage: %s [tag_name]" % argv[0]
 
 
 if __name__ == "__main__":
