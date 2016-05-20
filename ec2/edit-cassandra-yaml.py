c2517fee705a4e881b25f97af33586230ac0815b
Hobin Yoon <hobinyoon@gmail.com>
2016-05-20 23:39:23 +0000

    fix an error

--- ec2/edit-cassandra-yaml.py
+++ ec2/edit-cassandra-yaml.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-sys.path.insert(0, "%s/../../util/python" % os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, "%s/../util/python" % os.path.dirname(os.path.realpath(__file__)))
 import Cons
 import Util
 
