b00b06606781b754567fff8e8f74dd182fee41c4
Hobin Yoon <hobinyoon@gmail.com>
2016-06-01 16:28:52 -0400

    MT for measure time. No new-line version.

--- util/python/Cons.py
+++ util/python/Cons.py
@@ -1,7 +1,7 @@
-# Console output utility
-# - Not thread-safe. Doesn't guarantee ordering. It is by design
+# Console output utility. Not thread-safe by design
 
 import re
+import sys
 import time
 
 _ind_len = 0
@@ -30,7 +30,33 @@ def P(o, ind = 0):
 		_ind = _ind[: len(_ind) - ind]
 
 
-class MeasureTime:
+# No new-line
+def Pnnl(o, ind = 0):
+	global _ind_len, _ind
+	if ind > 0:
+		_ind_len += ind
+		for i in range(ind):
+			_ind += " "
+
+	if _ind_len > 0:
+		#print str(o).split("\n")
+		lines = str(o).split("\n")
+		for i in range(len(lines)):
+			if (i == len(lines) - 1) and (len(lines[i]) == 0):
+				continue
+			sys.stdout.write(_ind + lines[i])
+			sys.stdout.flush()
+	else:
+		sys.stdout.write(o)
+		sys.stdout.flush()
+
+	if ind > 0:
+		_ind_len -= ind
+		_ind = _ind[: len(_ind) - ind]
+
+
+# Measure time
+class MT:
 	def __init__(self, msg):
 		self.msg = msg
 
@@ -49,8 +75,26 @@ class MeasureTime:
 		_ind_len -= 2
 		_ind = _ind[: len(_ind) - 2]
 
-#	def GetMs(self):
-#		return (time.time() - self.start_time) * 1000.0
+
+# No new-line
+class MTnnl:
+	def __init__(self, msg):
+		self.msg = msg
+
+	def __enter__(self):
+		global _ind_len, _ind
+		Pnnl(self.msg)
+		_ind_len += 2
+		_ind += "  "
+		self.start_time = time.time()
+		return self
+
+	def __exit__(self, type, value, traceback):
+		global _ind_len, _ind
+		dur = time.time() - self.start_time
+		P("%.0f ms" % (dur * 1000.0))
+		_ind_len -= 2
+		_ind = _ind[: len(_ind) - 2]
 
 
 class Indent:
@@ -73,7 +117,7 @@ class Indent:
 def Test():
 	P("aa")
 
-	with MeasureTime("dkdkdk"):
+	with MT("dkdkdk"):
 		P(1.5)
 		P(True)
 
