3aa9a84cc3ad1fc6911bb45dd81074b32cc43c4a
Hobin Yoon <hobinyoon@gmail.com>
2016-06-08 15:25:07 -0400

    change s3 sync directory

--- ec2/batch-processing/S3.py
+++ ec2/batch-processing/S3.py
@@ -6,11 +6,10 @@ import Util
 
 
 def Sync():
-	dn = "%s/.s3" % os.path.dirname(__file__)
+	dn = "%s/work/acorn-data" % os.path.expanduser("~")
 	Util.MkDirs(dn)
 
 	# http://docs.aws.amazon.com/cli/latest/reference/s3/sync.html
-
 	cmd = "aws s3 sync s3://acorn-youtube %s" % dn
 	Util.RunSubp(cmd, shell = True, print_cmd = True, print_result = True)
 
