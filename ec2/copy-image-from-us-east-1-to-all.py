e4e1cdd0870f381cb0845ec244ef931632f85ee6
Hobin Yoon <hobinyoon@gmail.com>
2016-06-13 18:45:18 -0400

    copy AMIs

---
+++ ec2/copy-image-from-us-east-1-to-all.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+sys.path.insert(0, "%s/../util/python" % os.path.dirname(__file__))
+import Util
+
+
+def main(argv):
+	target_regions = [
+			"us-west-1"
+			, "us-west-2"
+			, "eu-west-1"
+			, "eu-central-1"
+			, "ap-southeast-1"
+			, "ap-southeast-2"
+			, "ap-northeast-1"
+			, "sa-east-1"
+			]
+
+	for r in target_regions:
+		cmd = "aws ec2 copy-image" \
+				" --source-image-id ami-f051949d" \
+				" --source-region us-east-1" \
+				" --region %s" \
+				" --name acorn-server-160613-1826" % r
+		Util.RunSubp(cmd)
+
+
+if __name__ == "__main__":
+	try:
+		sys.exit(main(sys.argv))
+	except KeyboardInterrupt:
+		print "\nInterrupted"
+		sys.exit(1)
