8e0739c4a40d370c5fdb01fe0e3c6add7a0db419
Hobin Yoon <hobinyoon@gmail.com>
2016-06-15 14:17:27 -0400

    updates

--- ec2/RegionToAmi.py
+++ ec2/RegionToAmi.py
@@ -1,14 +1,14 @@
 def GetLatestAmiId(region):
 	region_ami = {
-		"us-east-1": "ami-f051949d"
-		, "us-west-1"      : "ami-17480d77"
-		, "us-west-2"      : "ami-1d69937d"
-		, "eu-west-1"      : "ami-58e37c2b"
-		, "eu-central-1"   : "ami-27f71e48"
-		, "ap-southeast-1" : "ami-aba774c8"
-		, "ap-southeast-2" : "ami-bbe4cdd8"
-		, "ap-northeast-1" : "ami-a307ecc2"
-		, "sa-east-1"      : "ami-199c1675"
-		}
+			"ap-northeast-1": "ami-c94ca7a8"
+			, "ap-southeast-1": "ami-1bd40778"
+			, "ap-southeast-2": "ami-d2f6dfb1"
+			, "eu-central-1": "ami-9eda33f1"
+			, "eu-west-1": "ami-8b5ec1f8"
+			, "sa-east-1": "ami-80bf35ec"
+			, "us-east-1": "ami-abf83cc6"
+			, "us-west-1": "ami-e5602585"
+			, "us-west-2": "ami-3244be52"
+			}
 
 	return region_ami[region]
--- ec2/copy-image-from-us-east-1-to-all.py
+++ ec2/copy-image-from-us-east-1-to-all.py
@@ -8,6 +8,11 @@ import Util
 
 
 def main(argv):
+	if len(argv) != 3:
+		raise RuntimeError("Usage: %s ami-id-in-us-east-1 name\n" \
+				"  E.g.: %s ami-abf83cc6 acorn-server-160614-1331" \
+				% (argv[0], argv[0]))
+
 	target_regions = [
 			"us-west-1"
 			, "us-west-2"
@@ -19,13 +24,31 @@ def main(argv):
 			, "sa-east-1"
 			]
 
+	region_ami = {"us-east-1": argv[1]}
+
 	for r in target_regions:
 		cmd = "aws ec2 copy-image" \
-				" --source-image-id ami-f051949d" \
+				" --source-image-id %s" \
 				" --source-region us-east-1" \
 				" --region %s" \
-				" --name acorn-server-160613-1826" % r
-		Util.RunSubp(cmd)
+				" --name %s" \
+				% (argv[1], r, argv[2])
+		out = Util.RunSubp(cmd)
+		found_ami_id = False
+		for line in out.split("\n"):
+			if "\"ImageId\": \"ami-" in line:
+				t = line.split("\"ImageId\": \"")
+				if len(t) != 2:
+					raise RuntimeError("Unexpected line=[%s]" % line)
+				# ami-a46623c4"
+				# 012345678901
+				region_ami[r] = t[1][0:11+1]
+				found_ami_id = True
+				break
+		if not found_ami_id:
+			raise RuntimeError("Unexpected output=[%s]" % out)
+
+	print "{\n%s\n}" % ("\n, ".join(["\"%s\": \"%s\"" % (k, v) for (k, v) in sorted(region_ami.items())]))
 
 
 if __name__ == "__main__":
