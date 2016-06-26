4eb36cede9d3290163267cfc39ccf26e6d62e3d0
Hobin Yoon <hobinyoon@gmail.com>
2016-06-26 17:23:42 -0400

    new AMIs

--- ec2/RegionToAmi.py
+++ ec2/RegionToAmi.py
@@ -1,14 +1,14 @@
 def GetLatestAmiId(region):
 	region_ami = {
-			"ap-northeast-1": "ami-c94ca7a8"
-			, "ap-southeast-1": "ami-1bd40778"
-			, "ap-southeast-2": "ami-d2f6dfb1"
-			, "eu-central-1": "ami-9eda33f1"
-			, "eu-west-1": "ami-8b5ec1f8"
-			, "sa-east-1": "ami-80bf35ec"
-			, "us-east-1": "ami-abf83cc6"
-			, "us-west-1": "ami-e5602585"
-			, "us-west-2": "ami-3244be52"
+			"ap-northeast-1": "ami-687f8909"
+			, "ap-southeast-1": "ami-d1ee3cb2"
+			, "ap-southeast-2": "ami-dbd4fcb8"
+			, "eu-central-1": "ami-08bb5067"
+			, "eu-west-1": "ami-74be2507"
+			, "sa-east-1": "ami-bceb7ed0"
+			, "us-east-1": "ami-4e975823"
+			, "us-west-1": "ami-afcf8bcf"
+			, "us-west-2": "ami-75814715"
 			}
 
 	return region_ami[region]
