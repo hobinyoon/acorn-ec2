e287fe1d39e8b674ea7c2183d858dd0005ef17b8
Hobin Yoon <hobinyoon@gmail.com>
2016-07-04 23:54:45 -0400

    new AMIs

--- ec2/Ec2Region.py
+++ ec2/Ec2Region.py
@@ -1,16 +1,16 @@
 def GetLatestAmiId(region):
 	region_ami = {
-			"ap-northeast-1": "ami-daed1ebb"
-			, "ap-northeast-2": "ami-7a874d14"
-			, "ap-south-1": "ami-6d91fb02"
-			, "ap-southeast-1": "ami-70bd6013"
-			, "ap-southeast-2": "ami-9b3813f8"
-			, "eu-central-1": "ami-d047acbf"
-			, "eu-west-1": "ami-ec9cf99f"
-			, "sa-east-1": "ami-29188d45"
-			, "us-east-1": "ami-1a7dc70d"
-			, "us-west-1": "ami-ea92d58a"
-			, "us-west-2": "ami-1b40817b"
+			"ap-northeast-1": "ami-08b84a69"
+			, "ap-northeast-2": "ami-61915b0f"
+			, "ap-south-1": "ami-b8e68cd7"
+			, "ap-southeast-1": "ami-50d40933"
+			, "ap-southeast-2": "ami-fc13389f"
+			, "eu-central-1": "ami-9b8862f4"
+			, "eu-west-1": "ami-2c1d795f"
+			, "sa-east-1": "ami-ab33a6c7"
+			, "us-east-1": "ami-a742c7b0"
+			, "us-west-1": "ami-b9b1f6d9"
+			, "us-west-2": "ami-f58e4e95"
 			}
 
 	return region_ami[region]
